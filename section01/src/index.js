a782d0a6d3f5b43cdb52a56393ca48bb6b634627
tuan.nq <tuan.nq@flexidata.vn>
2017-06-20 11:02:51 +0700

    section-02: 20170620

--- section01/src/index.js
+++ section01/src/index.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyB0ZMOqT4D2H2YQJQtO4wGOtqga-9b5i_Y';
 
 /**
  * Create a new component
  * This component should produce some HTML
  */
-const App = () => {
-    return (
-        <div>
-            <SearchBar />    
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [], 
+            selectedVideo: null
+        };
+        YTSearch({ key: API_KEY, term: 'surfboard' }, (videos) => {
+            this.setState({ 
+                videos: videos, 
+                selectedVideo: videos[0]
+            });
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+
 }
 
 /**
