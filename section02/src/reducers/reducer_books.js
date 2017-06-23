016dd41abc41e05dab356b7166322d4c1b2d4a18
tuan.nq <tuan.nq@flexidata.vn>
2017-06-23 18:17:06 +0700

    section-04 20170620

---
+++ section02/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+	"use strict";
+	return [
+		{ title: 'Javascript: The Good Parts' },
+		{ title: 'Harry Potter' },
+		{ title: 'The Dark Tower' },
+		{ title: 'Eloquent Ruby' },
+	]
+}
---
+++ section02/webpack.config.js
@@ -0,0 +1,38 @@
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: [
+		'./src/index.js'
+	],
+	output: {
+		path: __dirname,
+		publicPath: '/',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [{
+			exclude: /node_modules/,
+			loader: 'babel',
+			query: {
+				presets: ['react', 'es2015', 'stage-1']
+			}
+		}, {
+			test: /\.less$/,
+			loader: ExtractTextPlugin.extract('style-loader', 'css!less'),
+		}, {
+			test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+			loader: 'file-loader'
+		}]
+	},
+	plugins: [
+		new ExtractTextPlugin('main.css')
+	],
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	devServer: {
+		historyApiFallback: true,
+		contentBase: './'
+	}
+};
